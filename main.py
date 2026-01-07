3627e582bac1711e698b636d57cdfca08c3a83b8
frontdev-corp8ai <sachinpal@Sachins-MacBook-Pro.local>
2026-01-07 22:20:48 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/health")
+def healthCheck():
+    return {"message": "Server running"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app",host="0.0.0.0",port=8000,reload=True )
